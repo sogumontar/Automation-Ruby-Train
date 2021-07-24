50b92235149b9bf868762c8055e84e1eaa9792e9
Hendra Simangunsong <hendrasimz92@gmail.com>
2021-07-24 23:33:13 +0800

    data-collector test

--- Amazon_test/Login Feature/features/step_definitions/login_step.rb
+++ Amazon_test/Login Feature/features/step_definitions/login_step.rb
@@ -6,12 +6,17 @@ driver = Selenium::WebDriver.for:chrome
 
 Given ("Open Amazon Site") do
     driver.navigate.to "https://amazon.com/"
+end
+
+Then("Open Login Page") do
     driver.find_element(:xpath,'//*[@id="nav-link-accountList"]/span').click
     sleep(1)
 end
 
 When ("Entry valid Email and Password") do
     driver.find_element(:xpath,'//*[@id="ap_email"]').send_keys 'hendrasimz92@gmail.com'
+end
+Then ("Entry Email and Password")do
     driver.find_element(:xpath, '//*[@id="continue"]').click
     driver.find_element(:xpath,'//*[@id="ap_password"]').send_keys 'Simangunsong77!'
 end
@@ -20,26 +25,6 @@ Then("Submit Form Login") do
     
     urlNya = driver.current_url
     puts urlNya
-    expect(urlNya).to eq("https://www.amazon.com/?ref_=nav_ya_signin&") 
-    
-end
-
-Given ("I Open Amazon Site") do
-    driver.navigate.to "https://amazon.com/"
-    driver.find_element(:xpath,'//*[@id="nav-link-accountList"]/span').click
-    sleep(1)
-end
-
-When ("Entry valid Email and invalid Password") do
-    driver.find_element(:xpath,'//*[@id="ap_email"]').send_keys 'hendrasimz92@gmail.com'
-    driver.find_element(:xpath, '//*[@id="continue"]').click
-    driver.find_element(:xpath,'//*[@id="ap_password"]').send_keys 'Simangunsong'
-end
-Then("I Submit Form Login") do
-    driver.find_element(:xpath, '//*[@id="signInSubmit"]').click
-    
-    urlNya = driver.current_url
-    puts urlNya
-    expect(urlNya).to eq("https://www.amazon.com/ap/signin")
+    expect(urlNya).not_to eq("https://www.amazon.com/?ref_=nav_ya_signin&")
     
 end
---
+++ Data-Collector-Test/features/Data-Collector-Feature.feature
@@ -0,0 +1,5 @@
+Feature: Search Data Collector
+    Scenario: Search Success
+        Given Open Data Collector
+        When Entry Number
+        Then Click Search
